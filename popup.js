2e0fc39235ba6ce9481ab45b745c2e5e3fbefaae
Brett <bmgeorg@g.clemson.edu>
2015-06-17 23:18:02 -0400

    Make config block expandable

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-//console.log(DirectionEnum.SLIDE_LEFT);
+$(function() {
+    $("#config_expand_button").click(function() {
+        // Eventually may change this to slideToggle(), but that's laggy right now
+        // Making elements fixed-width may help, but that stiffens the design too much right now
+        $("#config_block").toggle();
+    });
+});
