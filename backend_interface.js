78b57717ff3d50535b9590b982b883628f6fb6f7
Brett <bmgeorg@g.clemson.edu>
2015-06-19 16:24:40 -0400

    Add backend interface for retrieving smart folders

--- backend_interface.js
+++ backend_interface.js
@@ -3,4 +3,22 @@ var backend = {
     isSmartFolder: function(folderId) {
         return folderId%2 == 0;
     },
+    getSmartFolders: function() {
+        // name - string
+        // tagsString - array of string
+        function SmartFolder(name, tagsString) {
+            /* Public API */
+            this.name = function() { return this._name; };
+            this.tagsString = function() { return this._tagsString; };
+
+            /* Private API */
+            this._name = name;
+            this._tagsString = tagsString;
+        }
+
+        var smartFolders = [];
+        smartFolders.push(new SmartFolder("Programming", ["C", "C++", "Class", "Java"]));
+        smartFolders.push(new SmartFolder("Racquetball", ["Racquet", "tennis", "warehouse", "shoe"]));
+        return smartFolders;
+    },
 };
--- popup.js
+++ popup.js
@@ -1,7 +1,40 @@
 $(function() {
     $("#config_expand_button").click(function() {
-        // Eventually may change this to slideToggle(), but that's laggy right now
-        // Making elements fixed-width may help, but that stiffens the design too much right now
-        $("#config_block").toggle();
+        var configBlock = $("#config_block");
+        if(configBlock.is(":visible")) {
+            configBlock.hide();
+        } else {
+            loadSmartFolders(configBlock);
+            // Eventually I may change this to slideDown(), but that's laggy right now
+            // Making elements fixed-width may help, but that stiffens the design too much right now
+            configBlock.show();
+        }
     });
 });
+
+// container - jQuery
+function loadSmartFolders(container) {
+    var smartFolders = backend.getSmartFolders();
+    for(var i = 0; i < smartFolders.length; i++) {
+        var smartFolder = createSmartFolderBox(smartFolders[i]);
+        container.append(smartFolder);
+    }
+}
+
+// smartFolder - SmartFolder (see backend_interface)
+// returns - jQuery
+function createSmartFolderBox(smartFolder) {
+    var smartFolderBox = $('\
+    <div class="smart_folder_box box">\
+        <div class="smart_folder_name_row flex_row">\
+            <div class="lightbulb_folder_icon small_icon"></div>\
+            <input type="text" class="smart_folder_name full_height no_autofocus" value="' + smartFolder.name() + '" placeholder="Set folder name">\
+        </div>\
+        <div class="smart_folder_tags_row flex_row">\
+            <input type="text" class="full_height no_autofocus" value="' + smartFolder.tagsString() + '" placeholder="Tags (C++, Java, imgur)">\
+        </div>\
+        <div class="smart_folder_delete_button"></div>\
+    </div>\
+    ');
+    smartFolderBox.data("smartFolder", smartFolder);
+}
